c9c6d33ab17935517c2d167b833894b74f6ace1c
Iwn Elion <153987262+iwnelion@users.noreply.github.com>
2024-04-18 11:56:01 +0300

    Update int to string.py

--- int to string.py
+++ int to string.py
@@ -1,5 +1,3 @@
-Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 >>> x=input("give me a num")
 give me a num5
 >>> print(2*int(x))
@@ -9,8 +7,6 @@ give me a num5
 >>> type(x)
 <class 'str'>
 >>> y=int(x)
->>> print*y)
-SyntaxError: unmatched ')'
 >>> print(y)
 5
 >>> type(y)
