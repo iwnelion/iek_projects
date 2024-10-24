3e819932c6af1db6067aafd1d8b2eb4438fe9d42
joana <153987262+iwnelion@users.noreply.github.com>
2024-10-24 21:36:40 +0300

    Create triangle-lines.cpp

---
+++ openGL/triangle-lines.cpp
@@ -0,0 +1,36 @@
+// #include <windows.h>
+#include <GL/glut.h>
+#include <stdlib.h>
+
+void display(){
+    glClearColor(1,1,1,1);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_LINES);
+    glColor3f(1,0,0);
+    glVertex2i(20,20);
+    glVertex2i(40,40);
+
+    glColor3f(0,0,1);
+    glVertex2i(20,20);
+    glVertex2i(40,20);
+    
+    glVertex2i(40,40);
+    glVertex2i(40,20);
+
+    glEnd();
+    glFlush();
+}
+int main(int argc, char** argv){
+    glutInit(&argc,argv);
+    glutInitWindowPosition(50,50);
+    glutInitWindowSize(640,480);
+    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+    glutCreateWindow("a sample open gl application");
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(0,50,0,50);
+
+    glutDisplayFunc(display);
+    glutMainLoop(); 
+
+    return 0;
+}
