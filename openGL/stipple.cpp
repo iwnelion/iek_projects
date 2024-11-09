1819d65c9395e4264fad55e5d9e518af2580046b
joana <153987262+iwnelion@users.noreply.github.com>
2024-11-09 15:34:50 +0200

    Add files via upload

---
+++ openGL/stipple.cpp
@@ -0,0 +1,48 @@
+// #include <windows.h>
+#include <GL/glut.h>
+#include <stdlib.h>
+
+void display(){
+    glClearColor(1,1,1,0);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glColor3f(1,0,0);
+
+    glLineWidth(2);
+    glEnable(GL_LINE_STIPPLE);
+    glLineStipple(1,0x00FF);
+
+    glBegin(GL_LINES);
+    glVertex2i(0,0);
+    glVertex2i(10,0);
+    glVertex2i(0,10);
+    glVertex2i(10,10);
+    glEnd();
+
+    glColor3f(0,0,1);
+
+    glBegin(GL_LINE_LOOP);
+    glVertex2i(0,15);
+    glVertex2i(0,20);
+    glVertex2i(5,25);
+    glVertex2i(10,20);
+    glVertex2i(10,15);
+    glEnd();
+
+    glFlush();
+}
+int main(int argc, char** argv){
+    glutInit(&argc,argv);
+    glutInitWindowPosition(50,50);
+    glutInitWindowSize(640,480);
+    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGBA);
+    glutCreateWindow("drawing lines");
+
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(-5,30,-5,30);
+    glEnable(GL_LINE_STIPPLE);
+    glutDisplayFunc(display);
+    glutMainLoop();
+
+    return 0;
+}
