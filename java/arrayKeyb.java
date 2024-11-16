279d2524e3a862a42447e3ff0c888bc213a98155
joana <153987262+iwnelion@users.noreply.github.com>
2024-11-16 15:02:49 +0200

    Create arrayKeyb.java

---
+++ java/arrayKeyb.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class arrayKeyb{
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("dose onoma: ");
+        String name=scanner.nextLine();
+        System.out.println("name: "+name);
+    }
+}
+//scans name and prints it
