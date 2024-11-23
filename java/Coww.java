414034795e92c8c5b3e671d4fcdb65fa4de7dbf1
joana <153987262+iwnelion@users.noreply.github.com>
2024-11-23 11:22:59 +0200

    Create Coww.java

---
+++ java/Coww.java
@@ -0,0 +1,11 @@
+class Coww{
+    public static void main(String[] args){
+        Cow molly=new Cow();
+        molly.express();
+        System.out.println("molly's hunger->"+molly.hunger);
+        molly.eat();
+        molly.eat(4);
+        molly.peinaei();
+        molly.express();
+    }
+}
