b5fa2e0a555bd911d0ab3f7b120aed2532e2fc73
joana <153987262+iwnelion@users.noreply.github.com>
2024-12-07 16:30:06 +0200

    Create primary.php

---
+++ php/primary.php
@@ -0,0 +1,11 @@
+<?php
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    $a=$_POST["a"];
+    $b=$_POST["b"];
+    $result=-$b/$a;
+    if($b==0){
+        echo "invalid input";
+    }else{
+        echo "result: ".$result;
+    }
+}
